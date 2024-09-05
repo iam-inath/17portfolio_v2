5ae426da3d5dbddeb201c55b6a7a9a7796cd9ef2
Nathan Katshi <nathan@katshi.dev>
2024-09-05 15:01:42 +0200

    Update Notes.jsx

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,44 +2,44 @@ import React from 'react'
 
 function Notes() {
   return (
-    <div className="text-xs tracking-wide lg:text-xs">
+    <div className='text-xs tracking-wide lg:text-xs'>
       <p>
         Loosely designed in{' '}
-        <a href="https://www.figma.com">
-          <span className="text-slate-200 hover:text-secondary">Figma</span>{' '}
+        <a href='https://www.figma.com'>
+          <span className='text-slate-200 hover:text-secondary'>Figma</span>{' '}
         </a>
         and coded in{' '}
-        <a href="https://www.vscode.com">
-          <span className="text-slate-200 hover:text-secondary">
+        <a href='https://www.vscode.com'>
+          <span className='text-slate-200 hover:text-secondary'>
             Visual Studio Code
           </span>
         </a>{' '}
         by yours truly. Built with
-        <a href="https://www.react.dev">
-          <span className="text-slate-200 hover:text-secondary"> React.js</span>
+        <a href='https://www.react.dev'>
+          <span className='text-slate-200 hover:text-secondary'> React.js</span>
         </a>{' '}
         and{' '}
-        <a href="https://www.tailwindcss.com">
-          <span className="text-slate-200 hover:text-secondary">
+        <a href='https://www.tailwindcss.com'>
+          <span className='text-slate-200 hover:text-secondary'>
             {' '}
             Tailwind CSS
           </span>
         </a>{' '}
         , deployed with
-        <a href="https://www.netlify.com">
+        <a href='https://www.netlify.com'>
           {' '}
-          <span className="text-slate-200 hover:text-secondary">Netlify</span>
+          <span className='text-slate-200 hover:text-secondary'>Netlify</span>
         </a>{' '}
         . All text is set in the{' '}
-        <a href="https://www.inter.com">
-          <span className="text-slate-200 hover:text-secondary"> Inter</span>
+        <a href='https://www.inter.com'>
+          <span className='text-slate-200 hover:text-secondary'> Inter</span>
         </a>{' '}
         typeface.
       </p>
-      <p className="my-10 text-center">
-        Copyright {new Date().getFullYear()} •{' '}
-        <a href="https://www.katshi.dev">
-          <span className="text-slate-200 hover:text-secondary">
+      <p className='my-10 text-center'>
+        Copyright 2022 - {new Date().getFullYear()} •{' '}
+        <a href='https://www.katshi.dev'>
+          <span className='text-slate-200 hover:text-secondary'>
             Katshi.dev
           </span>
         </a>{' '}
