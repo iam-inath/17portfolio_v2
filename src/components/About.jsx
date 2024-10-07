260456bc0ba63ea709749bcd0aa76ab8b7e253f7
Nathan Katshi <nathan@katshi.dev>
2024-10-07 03:39:04 +0200

    Content Updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,35 +7,45 @@ function About() {
         About
       </h2>
       <p className=''>
-        Back in 2013, I decided to try my hand at building a strong foundation
-        in computer maintenance,{' '}
-        <span className=' text-slate-200'>
-          enterprise networking and systems engineering.{' '}
+        Back in 2013, I decided to build a strong foundation in computer
+        maintenance,{' '}
+        <span className='text-slate-200'>
+          network administration and systems engineering
         </span>
-        Over the years, I honed my skills in various tech domains, gaining
+        . Over the years, I honed my skills in various tech domains, gaining
         valuable experience in{' '}
-        <span className=' text-slate-200'>cloud technologies</span> and{' '}
-        <span className=' text-slate-200'>cybersecurity.</span> In 2021, I made
-        a significant shift to focus on development, driven by a passion for
-        creating intuitive and engaging user experiences.
-        <br />
-        <br /> Today, I specialize in front-end development, with a keen eye for
-        detail and a commitment to building user interfaces that are both
-        visually appealing and{' '}
-        <span className=' text-slate-200'>technically robust. </span> I’ve had
-        the pleasure of working on numerous projects, collaborating with diverse
-        teams to bring <span className=' text-slate-200'>innovative ideas</span>{' '}
+        <span className='text-slate-200'>cloud technologies</span> and{' '}
+        <span className='text-slate-200'>cybersecurity</span>.<br />
+        <br /> In 2021, I made a significant shift to focus on development,
+        driven by a passion for creating intuitive and engaging user
+        experiences. Today, I specialize in front-end development, with a strong
+        attention to detail and a commitment to building user interfaces that
+        are both visually appealing and{' '}
+        <span className='text-slate-200'>technically robust</span>. I've had the
+        pleasure of working on numerous projects, collaborating with diverse
+        teams to bring <span className='text-slate-200'>innovative ideas</span>{' '}
         to life. My expertise lies in blending design and functionality
         seamlessly, ensuring that every project I work on meets the highest
         standards of quality and usability.
         <br />
         <br />
-        When I’m not coding, I enjoy exploring new tech trends, reading, and
-        indulging in my hobbies. Whether it’s diving into a new book,
+        As a <span className='text-slate-200'>Cisco DevNet Enthusiast</span>, I
+        focus on network automation and programmability, leveraging these skills
+        to streamline and optimize network operations. Additionally, as part of
+        my <span className='text-slate-200'>Technical Writer</span> role, I
+        share my insights through technical articles on diverse topics, ranging
+        from front-end development best practices to network automation
+        techniques. One of the achievements I am the most proud of include being
+        recognized a{' '}
+        <span className='text-slate-200'>Cisco NetRiders Top Talent</span> in
+        2016.
+        <br /> <br />
+        When I'm not coding, I enjoy exploring new tech trends, reading, and
+        writing blogs and articles. Whether it's diving into a new book,
         experimenting with the latest tools, or simply enjoying some downtime,
-        I’m always eager to learn and grow. My journey in the tech world is
-        fueled by a continuous desire to push boundaries and create impactful
-        solutions.
+        I'm always learning and growing. Since the Sky is the limit, my journey
+        in the tech world is fueled by a continuous desire to push boundaries
+        and create impactful solutions.
       </p>
     </div>
   )
